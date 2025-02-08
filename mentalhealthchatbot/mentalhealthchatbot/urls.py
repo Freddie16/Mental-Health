c3ae333a999c2f25290ef43ab547f7c996066d2a
Freddie16 <freddiemurigi@gmail.com>
2025-02-08 13:39:55 +0300

    fixed auth

--- mentalhealthchatbot/mentalhealthchatbot/urls.py
+++ mentalhealthchatbot/mentalhealthchatbot/urls.py
@@ -5,4 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('chatbot.urls')),
+    path('accounts/', include('allauth.urls')),  # Ensure this line is present
+
 ]
